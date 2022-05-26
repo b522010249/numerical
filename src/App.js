5d395d88367755299fe3fa11044d9d03f1220d30
drof <ford009@hotmail.com>
2022-05-26 22:15:20 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react'
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
+// We use Route in order to define the different routes of our application
 
-function App() {
+//Root of Equation
+import Bisection from "./components/RoE/Bisection.js";
+import FalsePosition from "./components/RoE/FalsePosition.js";
+import NewtonRaphson from "./components/RoE/NewtonRaphson.js";
+import OnePointIteration from "./components/RoE/OnePointIteration.js";
+
+// We import all the components we need in our app
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+   //creating IP state
+   const [ip, setIP] = useState('');
+
+   //creating function to load ip address from the API
+   const getData = async () => {
+     const res = await axios.get('https://api.ipify.org/?format=json')
+     console.log(res.data);
+     setIP(res.data.ip)
+   }
+   
+   useEffect( () => {
+     //passing getData method to the lifecycle method
+     getData()
+ 
+   }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <Nav class="navbar navbar-dark bg-dark">
+        <Navbar className="Nav-content">
+          <Container>
+            <Navbar.Brand href="/">Numerical</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto" defaultActiveKey="/">
+                <NavDropdown title="Root of Equation" id="collasible-nav-dropdown" >
+                  <NavDropdown.Item as={Link} to="/Bisection">
+                    Bisection Methods
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/FalsePosition">
+                    False-Position
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/OnePointIteration">
+                    One-Point Iteration
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/NewtonRaphson">
+                    Newton Raphson
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Nav><Navbar.Brand>{ip}</Navbar.Brand></Nav>
+      </Nav>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Bisection />} />
+          <Route path="/Bisection" element={<Bisection />} />
+          <Route path="/FalsePosition" element={<FalsePosition />} />
+          <Route path="/OnePointIteration" element={<OnePointIteration />} />
+          <Route path="/NewtonRaphson" element={<NewtonRaphson />} />
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
